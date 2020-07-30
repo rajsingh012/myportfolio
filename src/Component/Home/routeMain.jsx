ba0dd1f2ad715cf226b8373c4dc99f17b8a5186b
rajsingh012 <uxraj1995@gmail.com>
2020-07-30 11:21:18 +0530

    UPDATE: icons added

--- src/Component/Home/routeMain.jsx
+++ src/Component/Home/routeMain.jsx
@@ -5,6 +5,7 @@ import AboutUs from './../About/index';
 import Portfolio from './../Portfolio/index';
 import ContactUs from './../Contact/index';
 import Skills from './../Tech/index';
+import Blog from './../Blog/index';
 
 const Main = () => (
     <Switch>
@@ -13,6 +14,7 @@ const Main = () => (
         <Route path='/portfolio' component={Portfolio} />
         <Route path='/contactus' component={ContactUs} />
         <Route path='/skills' component={Skills} />
+        <Route path='/blog' component={Blog} />
     </Switch>
 )
 
--- src/Component/Portfolio/index.jsx
+++ src/Component/Portfolio/index.jsx
@@ -1,33 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback, Fragment } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./photos";
 import portfolio from './../static/portfolio.json';
 import './../../css/_portfolio.less';
 
-class Portfolio extends Component {
-    render() {
-        return (
+const Portfolio = () => {
+
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
+    return (
+        <Fragment>
             <div className="rightbar__wrapper">
                 <div className="portfolio__section">
                     <h1 className="aboutus__section--heading">Awesome
                         <span className="aboutus__section--colortext">Portfolio</span>
                     </h1>
                     <div className="portfolio__section--boxwrap">
-                        {portfolio.map(data => {
-                            const { imgName = '', key = '', imgUrl = '' } = data;
-                            return (
-                                <div className="portfolio__section--imgsection" key={key}>
-                                    <img className="portfolio__section--imglist" src={imgUrl} />
-                                    <div className="portfolio__section--hoverview">
-                                        <span className="portfolio__section--plusicon"></span>
-                                        <h2>{imgName}</h2>
-                                    </div>
-                                </div>
-                            )
-                        })}
+                        <Gallery photos={photos} onClick={openLightbox} />
                     </div>
                 </div>
             </div>
-        )
-    }
+            <div>
+                <ModalGateway>
+                    {viewerIsOpen ? (
+                        <Modal onClose={closeLightbox}>
+                            <Carousel
+                                currentIndex={currentImage}
+                                views={photos.map(x => ({
+                                    ...x,
+                                    srcset: x.srcSet,
+                                    caption: x.title
+                                }))}
+                            />
+                        </Modal>
+                    ) : null}
+                </ModalGateway>
+            </div>
+        </Fragment>
+    )
 }
 
 export default Portfolio;
