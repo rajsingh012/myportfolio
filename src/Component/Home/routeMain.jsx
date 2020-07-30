0aaa11af823ae3c616609d8fd2c2600ed6a6d4b4
rajsingh012 <uxraj1995@gmail.com>
2020-07-30 17:37:47 +0530

    UPDATE

--- src/Component/Home/routeMain.jsx
+++ src/Component/Home/routeMain.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Home from './rightbar';
-import AboutUs from './../About/index';
-import Portfolio from './../Portfolio/index';
-import ContactUs from './../Contact/index';
-import Skills from './../Tech/index';
-import Blog from './../Blog/index';
+import React, { Suspense, lazy } from 'react'
+import { Switch, Route } from 'react-router-dom';
+import './../../css/_loader.less';
+const Home = lazy(() => import('./rightbar'));
+const AboutUs = lazy(() => import('./../About/index'));
+const Portfolio = lazy(() => import('./../Portfolio/index'));
+const ContactUs = lazy(() => import('./../Contact/index'));
+const Skills = lazy(() => import('./../Tech/index'));
+const Blog = lazy(() => import('./../Blog/index'));
 
 const Main = () => (
-    <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/aboutus' component={AboutUs} />
-        <Route path='/portfolio' component={Portfolio} />
-        <Route path='/contactus' component={ContactUs} />
-        <Route path='/skills' component={Skills} />
-        <Route path='/blog' component={Blog} />
-    </Switch>
+    <Suspense fallback={<div className="loader__wrap">Loading...</div>}>
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/aboutus' component={AboutUs} />
+            <Route path='/portfolio' component={Portfolio} />
+            <Route path='/contactus' component={ContactUs} />
+            <Route path='/skills' component={Skills} />
+            <Route path='/blog' component={Blog} />
+        </Switch>
+    </Suspense>
 )
 
 export default Main
